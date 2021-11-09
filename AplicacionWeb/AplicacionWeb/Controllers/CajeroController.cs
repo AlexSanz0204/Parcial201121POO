f1f45adf44b0b5e0e0800d659d2e019ae49b3b8f
AlexSanz02 <zs0128032021@unab.edu.sv>
2021-11-08 19:34:52 -0600

    Parcial2POO

---
+++ AplicacionWeb/AplicacionWeb/Controllers/CajeroController.cs
@@ -0,0 +1,52 @@
+﻿using AplicacionWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AplicacionWeb.Controllers
+{
+    public class CajeroController : Controller
+    {
+        // GET: Persona
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult PersonaVista() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult PersonaVista(Usuario p)
+        {
+            Console.WriteLine(p.Usuario);
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult ClaseHtml() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ClaseHtml(Usuario p)
+        {
+            _ = p;
+            if (p.Usuario.Equals("Alexander") && p.Contraseña.Equals("020418")) 
+            {
+                return Redirect("Bienvenido");
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Bienvenida() 
+        {
+            return View();
+        }
+    }
+}
---
+++ AplicacionWeb/AplicacionWeb/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AplicacionWeb.Models
+{
+    public class Usuario
+    {
+        public String Usuario { get; set; }
+
+        public String Contraseña { get; set; }
+
+        
+
+
+    }
+}
